99a367a4b2fb2ee15408e81b9f5cd78f2e52d521
jefbar1974 <jefbar@gmail.com>
2022-10-04 10:58:18 -0300

    Modificando js

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 
 
 function validarEChamarAPI() {
-    console.log("Chegou")
+    
 const value1 = document.querySelector ("#input1").value
 const value2 = document.querySelector ("#input2").value
+
+
+
+document.querySelector("#ressucesso").style.display = "block"
+
 }
+
+
+
+
+
